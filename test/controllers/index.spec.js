8cf0174b98eb376c04b3fb9950375887d5c2faba
Carles Galan Cladera <cgcladera@gmail.com>
2016-03-11 11:38:10 +0100

    Creates hello world

--- test/controllers/index.spec.js
+++ test/controllers/index.spec.js
@@ -4,12 +4,12 @@ var request = require('superagent');
 var expect = require('expect.js');
 
 describe('Index controller', function() {
-  it('should respond 200 ok', function(done) {
+  it('should respond 200 Hello world', function(done) {
     request
       .get('http://localhost:9000/')
       .end(function(err, res) {
         expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Ok!');
+        expect(res.text).to.equal('Hello world');
         done();
       });
   });
